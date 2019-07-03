90e325e57cd66bb554cbeaa14837b5a37bbc3090
Cole Proctor <proctorcole@gmail.com>
2019-07-02 17:48:44 -0700

    Leading into upload file

--- upload.php
+++ upload.php
@@ -7,4 +7,24 @@ if(isset($_POST['submit'])){
     $fileSize = $_FILES['file']['size'];
     $fileError = $_FILES['file']['error'];
     $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('txt');
+    if(in_array($fileActualExt, $allowed)){
+        if($fileError === 0)
+        {
+            $fileNameNew = uniqid('', true).".".$fileActualExt;
+            $fileDestination = 'uploads/'.$fileNameNew;
+            move_uploaded_file($fileTmpName, $fileDestination);
+            header("Location: mainWindow.html");
+        }
+        else{
+            echo "There was an error uploading the file.";
+        }
+    }
+    else{
+        echo "Please upload a txt file.";
+    }
 }
